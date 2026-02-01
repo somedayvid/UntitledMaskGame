58bd20f1b7299626e85072171f985f5ee6cb05b0
TChoy04 <tszfai04@gmail.com>
2026-01-31 23:57:43 -0500

    deck

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
-    [SerializeField] List<Card> deck;
-    List<Card> discard;
+    [SerializeField] List<Card> deck = new List<Card>();
+    List<Card> discard = new List<Card>();
     void shuffle()
     {
         for (int i = deck.Count - 1; i > 0; i--)
@@ -20,10 +20,11 @@ public class DeckManager : MonoBehaviour
     {
         if (deck.Count == 0)
         {
-            deck = discard;
+            deck = new List<Card>(discard);
             discard.Clear();
             shuffle();
         }
+        if (deck.Count == 0) return null;
         Card card = deck[0];
         deck.RemoveAt(0);
         discard.Add(card);
@@ -36,7 +37,19 @@ public class DeckManager : MonoBehaviour
     }
     void Start()
     {
-        shuffle();  
+        Card newCard = new Card();
+        newCard.cardEffect = CardEffect.DrunkenFist;
+        AddCard(newCard);
+        Card newCard1 = new Card();
+        newCard1.cardEffect = CardEffect.Momentum;
+        AddCard(newCard1);
+        Card newCard2 = new Card();
+        newCard2.cardEffect = CardEffect.RockThrow;
+        AddCard(newCard2);
+        shuffle();
+        Debug.Log(Draw().GetName());
+        Debug.Log(Draw().GetName());
+        Debug.Log(Draw().GetName());
     }
 
     // Update is called once per frame
