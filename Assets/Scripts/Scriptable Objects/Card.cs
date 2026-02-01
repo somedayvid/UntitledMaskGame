db3e0bbb01b6ef6e5d679fe0eb207d98a0801726
TChoy04 <tszfai04@gmail.com>
2026-01-31 20:25:24 -0500

    terra

--- Assets/Scripts/Scriptable Objects/Card.cs
+++ Assets/Scripts/Scriptable Objects/Card.cs
@@ -44,13 +44,14 @@ public class Card
     public string cardName;
     public CardType cardType;
     public CardEffect cardEffect;
-
+    public int chi;
     // define your card properties here
     public int damage;
     public int shield;
     public Card()
     {
         damage = 5;
+        chi = 1;
         cardType = CardType.Attack;
     }
     //example I will try something - Ricky
@@ -69,20 +70,24 @@ public class Card
                 damage = 5;
                 break;
             case (CardEffect.SmallShieldPotion):
+                chi = 1;
                 shield = 5;
                 cardType = CardType.Defense;
                 break;
             case (CardEffect.ShieldPotion):
+                chi = 2;
                 shield = 25;
                 cardType = CardType.Defense;
                 break;
             case (CardEffect.OrientalMedicineJug):
+                chi = 3;
                 shield = 50;
                 cardType = CardType.Defense;
                 break;
             case (CardEffect.OrientalDaggerRitual):
                 break;
             case (CardEffect.OrientalDagger):
+                chi = 0;
                 damage = 3;
                 break;
             case (CardEffect.Meditate):
@@ -98,6 +103,7 @@ public class Card
             case (CardEffect.SunTzusInsight):
                 break;
             case (CardEffect.DragonStrike):
+                damage = 5;
                 break;
             case (CardEffect.HeavenSplit):
                 damage = 5;
@@ -107,6 +113,8 @@ public class Card
             case (CardEffect.Momentum):
                 break;
             case (CardEffect.RockThrow):
+                damage = 3;
+                chi = 0;
                 break;
             case (CardEffect.Pills):
                 break;
---
+++ Assets/Scripts/Terracotta.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terracotta : MonoBehaviour, Enemy
+
+{
+    int maxHealth = 30;
+    int shield = 0;
+    int health = 0;
+    void TakeDamage(int amount)
+    {
+        if (shield>0)
+        {
+            shield -= amount;
+            if (shield < 0) amount = shield * -1;
+        }
+        health = Mathf.Max(health-amount, 0);
+    }
+    bool IsAlive()
+    {
+        return health > 0;
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ResolveAction(Player player)
+    {
+        if (player == null) return;
+        if (!player.IsAlive()) return;
+
+        int rng = Random.Range(0, 2);
+        if (rng == 1) player.TakeDamage(7);
+        else shield += 5;
+    }
+
+    void Enemy.TakeDamage(int amount)
+    {
+        TakeDamage(amount);
+    }
+
+    bool Enemy.IsAlive()
+    {
+        return IsAlive();
+    }
+}
